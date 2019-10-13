9f0e308a7f9c90fb73a194a15e1db674a064a484
ukkpaapay <38246106+ukkpaapay@users.noreply.github.com>
2019-10-14 00:12:33 +0700

    ต่อfirebase

---
+++ somefood.js
@@ -0,0 +1,9 @@
+var signin = function(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if(username != "ukkpaapay" && password != "55659900"){
+
+    }else{
+        window.location.replace("foodcategory.html");
+    }
+}
