5b683c6caa111a19a22b8dca1c9dde1c0b9c6acd
ukkpaapay <38246106+ukkpaapay@users.noreply.github.com>
2019-10-14 19:23:35 +0700

    เพิ่มfirebaseconfig

--- somefood.js
+++ somefood.js
@@ -7,3 +7,21 @@ var signin = function(){
         window.location.replace("foodcategory.html");
     }
 }
+
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCMEn-5Gdqf7-HJTtwY_Hgf2GoIN8jGvX4",
+    authDomain: "somefood-f12c4.firebaseapp.com",
+    databaseURL: "https://somefood-f12c4.firebaseio.com",
+    projectId: "somefood-f12c4",
+    storageBucket: "somefood-f12c4.appspot.com",
+    messagingSenderId: "64010995887",
+    appId: "1:64010995887:web:e9ec8907117862d7772c5a",
+    measurementId: "G-ZM248P04ES"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  var db = firebase.firestore();
