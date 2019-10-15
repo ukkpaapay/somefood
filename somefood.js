faedddda27b6c9476c1aa77a0169c40706148dcc
ukkpaapay <38246106+ukkpaapay@users.noreply.github.com>
2019-10-16 00:50:57 +0700

    ทำระบบlogin

--- somefood.js
+++ somefood.js
@@ -9,11 +9,32 @@ var firebaseConfig = {
   appId: "1:64010995887:web:e9ec8907117862d7772c5a",
   measurementId: "G-ZM248P04ES"
 };
+
+
 firebase.initializeApp(firebaseConfig);
 
 var db = firebase.firestore();
 
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+    console.log(`${email} User is signed in.`);
+    $("#content")[0].load("foodcategory.html");
+    // ...
+  } else {
+    // User is signed out.
+    // ...
+    console.log("User is signed out.");
 
+  }
+});
 
 document.addEventListener('init', function (event) {
   var page = event.target;
@@ -31,29 +52,66 @@ document.addEventListener('init', function (event) {
         console.log(errorCode);
         console.log(errorMessage);
       });
-      firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-          // User is signed in.
-          var displayName = user.displayName;
-          var email = user.email;
-          var emailVerified = user.emailVerified;
-          var photoURL = user.photoURL;
-          var isAnonymous = user.isAnonymous;
-          var uid = user.uid;
-          var providerData = user.providerData;
-          console.log(`${email} User is signed in.`);
-          $("#content")[0].load("foodcategory.html");
-          // ...
-        } else {
-          // User is signed out.
-          // ...
-          console.log("User is signed out.");
-          
-        }
+
+    });
+
+    $("#register").click(function () {
+      // $("#content")[0].load("register.html");
+      firebase.auth().signOut().then(function () {
+        console.log("logout");
+        // Sign-out successful.
+      }).catch(function (error) {
+        // An error happened.
       });
     });
+
+    $("#googlelogin").click(function () {
+      var provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithPopup(provider).then(function (result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+      }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        console.log(error);
+        
+        // ...
+      });
+
+    });
+
   }
 });
+
+if (page.id === 'homePage') {
+  db.collection("restaurant").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      //object
+      console.log(`${doc.id} => ${doc.data()}`);
+      // each
+      console.log("name : " + doc.data().name);
+      console.log("id : " + doc.data().id);
+      var carousel = `<ons-carousel-item modifier="nodivider" id="item1" class="recomended_item">
+              <div class="thumbnail" style="background-image: url( ${doc.data().photourl} )">
+              </div>
+              <div class="recomended_item_title" id="item1_name">${doc.data().name}</div>
+              </ons-carousel-item>`;
+      $('#carousel').append(carousel);
+      //` ` for ต่อ string
+
+    });
+  });
+}
+
+
 
 
 
