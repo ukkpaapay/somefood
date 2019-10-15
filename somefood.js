6c0e59a91c020467a849d95b4eb84e7236dcdb12
ukkpaapay <38246106+ukkpaapay@users.noreply.github.com>
2019-10-15 22:48:07 +0700

    แก้บัคและทำระบบloginด้วยemail

--- somefood.js
+++ somefood.js
@@ -1,27 +1,72 @@
-var signin = function(){
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    if(username != "ukkpaapay@hotmail.com" && password != "55659900"){
 
-    }else{
-        window.location.replace("foodcategory.html");
-    }
-}
+var firebaseConfig = {
+  apiKey: "AIzaSyCMEn-5Gdqf7-HJTtwY_Hgf2GoIN8jGvX4",
+  authDomain: "somefood-f12c4.firebaseapp.com",
+  databaseURL: "https://somefood-f12c4.firebaseio.com",
+  projectId: "somefood-f12c4",
+  storageBucket: "somefood-f12c4.appspot.com",
+  messagingSenderId: "64010995887",
+  appId: "1:64010995887:web:e9ec8907117862d7772c5a",
+  measurementId: "G-ZM248P04ES"
+};
+firebase.initializeApp(firebaseConfig);
 
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyCMEn-5Gdqf7-HJTtwY_Hgf2GoIN8jGvX4",
-    authDomain: "somefood-f12c4.firebaseapp.com",
-    databaseURL: "https://somefood-f12c4.firebaseio.com",
-    projectId: "somefood-f12c4",
-    storageBucket: "somefood-f12c4.appspot.com",
-    messagingSenderId: "64010995887",
-    appId: "1:64010995887:web:e9ec8907117862d7772c5a",
-    measurementId: "G-ZM248P04ES"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  
-  var db = firebase.firestore();
+var db = firebase.firestore();
 
-  
+
+
+document.addEventListener('init', function (event) {
+  var page = event.target;
+
+  if (page.id === 'loginPage') {
+
+    $("#signin").click(function () {
+      var username = $("#username").val();
+      var password = $("#password").val();
+      firebase.auth().signInWithEmailAndPassword(username, password).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+        console.log(errorCode);
+        console.log(errorMessage);
+      });
+      firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+          // User is signed in.
+          var displayName = user.displayName;
+          var email = user.email;
+          var emailVerified = user.emailVerified;
+          var photoURL = user.photoURL;
+          var isAnonymous = user.isAnonymous;
+          var uid = user.uid;
+          var providerData = user.providerData;
+          console.log(`${email} User is signed in.`);
+          $("#content")[0].load("foodcategory.html");
+          // ...
+        } else {
+          // User is signed out.
+          // ...
+          console.log("User is signed out.");
+          
+        }
+      });
+    });
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
