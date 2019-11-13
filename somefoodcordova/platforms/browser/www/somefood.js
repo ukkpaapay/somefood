16a582166102f821a58889cb861d158b1a78e630
ukkpaapay <38246106+ukkpaapay@users.noreply.github.com>
2019-11-13 14:38:34 +0700

    cordova ba

---
+++ somefoodcordova/platforms/browser/www/somefood.js
@@ -0,0 +1,359 @@
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCMEn-5Gdqf7-HJTtwY_Hgf2GoIN8jGvX4",
+  authDomain: "somefood-f12c4.firebaseapp.com",
+  databaseURL: "https://somefood-f12c4.firebaseio.com",
+  projectId: "somefood-f12c4",
+  storageBucket: "somefood-f12c4.appspot.com",
+  messagingSenderId: "64010995887",
+  appId: "1:64010995887:web:e9ec8907117862d7772c5a",
+  measurementId: "G-ZM248P04ES"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore();
+
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+    console.log(`${email} User is signed in.`);
+    $("#content")[0].load("home.html");
+    // ...
+  } else {
+    // User is signed out.
+    // ...
+    console.log("User is signed out.");
+
+  }
+});
+
+function selectmenu(resid) {
+  id = resid;
+  console.log(id);
+  localStorage.setItem("selectedRestId", id)
+  $("#content")[0].load("resturantlist.html");
+
+}
+
+var it = [];
+var pr = [];
+var itpr = parseInt(0);
+function order(item, price) {
+  ons.notification.alert("Add to card !");
+  it.push(item);
+  pr.push(price);
+  itpr += parseInt(price);
+  it.forEach(element => {
+    console.log(element);
+  });
+
+}
+
+document.addEventListener('init', function (event) {
+  var page = event.target;
+  console.log("now at:", page.id);
+
+  if (page.id === 'loginPage') {
+
+    $("#signin").click(function () {
+      var username = $("#username").val();
+      var password = $("#password").val();
+      firebase.auth().signInWithEmailAndPassword(username, password).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+        console.log(errorCode);
+        console.log(errorMessage);
+      });
+
+    });
+
+
+    $("#googlelogin").click(function () {
+      var provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithPopup(provider).then(function (result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+      }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        console.log(error);
+
+        // ...
+      });
+
+    });
+
+    $("#register").click(function () {
+      $("#content")[0].load("register.html");
+    });
+
+  }
+
+  if (page.id == 'register') {
+    $('#SignUp').click(function () {
+      var email = document.getElementById('EmailAddress').value;
+      var password = document.getElementById('Password').value;
+      //var fullname = document.getElementById('fullnamesignup').value;
+      //var phone = document.getElementById('phonenumbersignup').value;
+      if (email.length < 4) {
+        alert('Please enter an email address.');
+        return;
+      }
+      if (password.length < 4) {
+        alert('Please enter a password.');
+        return;
+      }
+      firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // [START_EXCLUDE]
+        if (errorCode == 'auth/weak-password') {
+          alert('The password is too weak.');
+        } else {
+          alert(errorMessage);
+        }
+        console.log(error);
+        // [END_EXCLUDE]
+      });
+    });
+  }
+
+  if (page.id === 'homePage') {
+    $("#carousel").empty();
+    db.collection("res").get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        var item = `<ons-carousel-item modifier="nodivider" onclick="selectmenu('${doc.id}')" class="recomended_item">
+              <div  class="thumbnail" style="background-image: url('${doc.data().url}')"></div>           
+              <div class="recomended_item_title" id="item1_${doc.data().id}">${doc.data().name}</div>
+          </ons-carousel-item>`
+        $("#carousel").append(item);
+      });
+    });
+
+
+    $("#ffbtn").click(function () {
+      localStorage.setItem("selectedCategory", "res");
+      $("#content")[0].load("resturantlist.html");
+    });
+
+    $("#drinkbtn").click(function () {
+      localStorage.setItem("selectedCategory", "drink");
+      $("#content")[0].load("resturantlist.html");
+    });
+
+    $("#menubtn").click(function () {
+      $("#sidemenu")[0].open();
+    });
+
+    $("#carousel").click(function () {
+      $("#content")[0].load("resturantmenu.html");
+    });
+
+  }
+
+  if (page.id === 'restureantlist') {
+    var category = localStorage.getItem("selectedCategory");
+
+
+    $("#list").empty();
+    db.collection(category).get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          var item = `
+         
+          <ons-card>
+        <ons-row >
+                    <img src="${doc.data().url}" height="100" width="100">
+                <ons-col style="margin-left:5% "><p>${doc.data().name}</p></ons-col-8>
+
+                <ons-col  align="right"><ons-button style="background-color: gray;" onclick="selectmenu('${doc.id}')">Menu</ons-button></ons-col-4>
+            </ons-row>
+    </ons-card>
+         
+         `
+          $("#list").append(item);
+
+
+        });
+      });
+
+    $("#list").click(function () {
+      $("#content")[0].load("resturantmenu.html");
+    });
+
+    $("#backbtn").click(function () {
+      $("#content")[0].load("home.html");
+    });
+
+  }
+
+  if (page.id === 'menuPage') {
+
+
+    $("#logout").click(function () {
+      // $("#content")[0].load("register.html");
+      firebase.auth().signOut().then(function () {
+        console.log("logout");
+        // Sign-out successful.
+      }).catch(function (error) {
+        // An error happened.
+      });
+      $("#content")[0].load("login.html");
+    });
+
+    $("#Cart").click(function () {
+      $("#content")[0].load("orderconfirmation.html");
+      $("#sidemenu")[0].close();
+    });
+
+    $("#home").click(function () {
+      $("#content")[0].load("home.html");
+      $("#sidemenu")[0].close();
+    });
+  }
+
+  if (page.id === 'restureantmenu') {
+    var catagorymenu = localStorage.getItem("selectedCategory");
+    var selectedRestId = localStorage.getItem("selectedRestId");
+    $("#foods").empty();
+    $("#photo").empty();
+    var docRef = db.collection(catagorymenu).doc(selectedRestId);
+    var photo =
+    docRef.get().then(function (doc) {
+      if (doc.exists) {
+        var photo = `<ons-row ><img class ="img" src="${doc.data().url}" style="width: 50% ;" class="center"></img><ons-col  align="right"><h3 style="color: green;">open</h3></ons-col-4>
+        </ons-row>`
+        doc.data().menu.forEach(item => {
+          var item = ` <ons-card style="background-color:${doc.data().color};"  onclick="order('${item.name}','${item.price}')">
+          
+      <ons-row >
+          <ons-col ><h3>${item.name}</h3></ons-col-8>
+
+          <ons-col  align="right">${item.price} baht</ons-col-4>
+      </ons-row>
+  </ons-card>`
+          $("#foods").append(item);
+        });
+        $("#photo").append(photo);
+
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    }).catch(function (error) {
+      console.log("Error getting document:", error);
+    });
+
+
+    $("#back").click(function () {
+      $("#content")[0].load("home.html");
+    });
+
+  }
+
+  if (page.id === 'orderconfirmation') {
+    for (var i = 0; i < it.length; i++) {
+      var item = `
+      <ons-list-item>
+      <div class="left">${it[i]}</div>
+      <div class="right">${pr[i]}</div>
+      </ons-list-item>`
+      $("#food").append(item);
+    }
+    $("#Total").append(itpr);
+    $(function () {
+      $("#back").click(function () {
+        $("#content")[0].load("home.html");
+      });
+    })
+
+    $("#cfbtn").click(function () {
+      
+      if(pr == parseInt(0)){
+        ons.notification.alert("No menu !");
+      }else{
+        ons.notification.alert("Order sucess !");
+      pr = parseInt(0);
+      it = parseInt(0);
+      it = [];
+      pr = [];
+      $("#content")[0].load("home.html");
+    
+    }
+    });
+    
+    $("#cfbtn1").click(function () {
+      
+      if(pr == parseInt(0)){
+        ons.notification.alert("No menu !");
+      }else{
+        ons.notification.alert("Order sucess !");
+      pr = parseInt(0);
+      it = parseInt(0);
+      it = [];
+      pr = [];
+      $("#content")[0].load("home.html");
+    
+    }
+    });
+
+    $("#cfbtn2").click(function () {
+      
+      if(pr == parseInt(0)){
+        ons.notification.alert("No menu !");
+      }else{
+        ons.notification.alert("Order sucess !");
+      pr = parseInt(0);
+      it = parseInt(0);
+      it = [];
+      pr = [];
+      $("#content")[0].load("home.html");
+    
+    }
+    });
+
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
